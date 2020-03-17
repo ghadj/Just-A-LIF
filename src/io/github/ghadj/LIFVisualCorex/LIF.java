402a8af3dae799acaa567dda01657f1b4d60f333
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2020-03-17 23:26:31 +0200

    implementation of main functions

--- src/io/github/ghadj/LIFVisualCorex/LIF.java
+++ src/io/github/ghadj/LIFVisualCorex/LIF.java
@@ -1,16 +1,92 @@
 package io.github.ghadj.LIFVisualCorex;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * Implementation of generic Leaky Integrate-and-Fire model.
+ * 
  * @author George Hadjiantonis
- *
+ * @since 17-03-2020
  */
 public class LIF {
+	private double tau_m; // membrane time constant - msec
+	private double Vreset; // mV
+	private double Vrest; // aka reversal potential of the leakage (E_L) - mV
+	private double Vth; // mV
+	private double Rm; // membrane resistance - MΩ
+	private double Trefract; // absolute refractory period - msec
+
+	private ArrayList<Double> spikes = null; // time of each spike
+	private ArrayList<Double> potential = null; // potential at a current time
 
 	/**
 	 * 
+	 * @param tau_m
+	 * @param Vreset
+	 * @param Vrest
+	 * @param Vth
+	 * @param Rm
+	 * @param Trefract
 	 */
-	public LIF() {
-		// TODO Auto-generated constructor stub
+	public LIF(double tau_m, double Vreset, double Vrest, double Vth, double Rm, double Trefract) {
+		this.tau_m = tau_m;
+		this.Vreset = Vreset;
+		this.Vrest = Vrest;
+		this.Vth = Vth;
+		this.Rm = Rm;
+		this.Trefract = Trefract;
 	}
 
+	/**
+	 * V(t) = E_L + R_m * I_e + (V(0) − E_L − R_m * I_e ) * exp(−t/τ_m )
+	 * 
+	 * @param V0
+	 * @param Ie
+	 * @param duration
+	 * @param dt
+	 */
+	public void run(double V0, double Ie, double duration, double dt) {
+		spikes = new ArrayList<Double>((int) Math.ceil(duration / dt));
+		potential = new ArrayList<Double>((int) Math.ceil(duration / dt));
+		double Vt = V0;
+		for (double t = 0; t < duration; t += dt) {
+			potential.add(Vt);
+			Vt = Vrest + (Rm * Ie) + (V0 - Vrest - (Rm * Ie)) * Math.exp(-t / tau_m);
+			if (Vt >= Vth) {
+				spikes.add(t);
+				potential.add(10.0); // high value to show spike
+				Vt = Vreset;
+				double refractPeriod = t + Trefract;
+				for (t += dt; t < refractPeriod; t += dt)
+					potential.add(Vt);
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public List<Double> getSpikes() {
+		return spikes;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public List<Double> getPotential() {
+		return potential;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public int numSpikes() {
+		if (spikes != null)
+			return spikes.size();
+		return -1;
+	}
 }
--- src/io/github/ghadj/LIFVisualCorex/LIFDriver.java
+++ src/io/github/ghadj/LIFVisualCorex/LIFDriver.java
@@ -1,17 +1,44 @@
 package io.github.ghadj.LIFVisualCorex;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 /**
+ * Driver for LIF class.
+ * 
  * @author George Hadjiantonis
- *
+ * @since 17-03-2020
  */
 public class LIFDriver {
+	// Set Constants
+	public static final double tau_m = 30; // membrane time constant - msec
+	public static final double Vreset = -65; // reset potential - mV
+	public static final double Vrest = Vreset; // aka reversal potential of the leakage (E_L) - mV
+	public static final double Vth = -50; // threshold potential - mV
+	public static final double V0 = -67; // Membrane potential at time 0 - mV
+	public static final double Rm = 90; // membrane resistance - MΩ
+	public static final double Ie = 1; // constant injected current - nA
+	public static final double Trefract = 2; // absolute refractory period - msec
+	public static final double Dt = 0.25; // Δt - time between two consecutive points
+	public static final double duration = 1000; // duration of the simulation - msec
 
 	/**
 	 * @param args
+	 * @throws IOException
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		LIF model = new LIF(tau_m, Vreset, Vrest, Vth, Rm, Trefract);
 
+		model.run(V0, Ie, duration, Dt);
+		System.out.println("spikes #" + model.numSpikes());
+
+		FileWriter writer = new FileWriter("./ouput.csv");
+		List<Double> potential = model.getPotential();
+		if (potential != null) {
+			String output = potential.toString().replaceAll("\\[|\\]", "").replaceAll(",", "\n");
+			writer.write(output);
+		}
+		writer.close();
 	}
-
 }
