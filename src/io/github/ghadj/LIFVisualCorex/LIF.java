2a04a09d661c4c210a847c66ff6f376159883a83
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2020-03-17 13:31:58 +0200

    initial commit

---
+++ src/io/github/ghadj/LIFVisualCorex/LIF.java
@@ -0,0 +1,16 @@
+package io.github.ghadj.LIFVisualCorex;
+
+/**
+ * @author George Hadjiantonis
+ *
+ */
+public class LIF {
+
+	/**
+	 * 
+	 */
+	public LIF() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/io/github/ghadj/LIFVisualCorex/LIFDriver.java
@@ -0,0 +1,17 @@
+package io.github.ghadj.LIFVisualCorex;
+
+/**
+ * @author George Hadjiantonis
+ *
+ */
+public class LIFDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
