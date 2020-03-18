dd03c602849f83cfa19907e2025fe51313326ba5
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2020-03-19 00:04:40 +0200

    minor fixes

--- LIF.py
+++ LIF.py
@@ -9,13 +9,13 @@ N = 1               # number of neurons
 tau_m = 30 * ms     # membrane time constant
 v_r = -65 * mV      # reset potential
 v_th = -50 * mV     # threshold potential
-v_rest = -65*mV     # rest potential
+v_rest = -65 * mV   # rest potential
 Rm_Ie =  90 * mV    # membrane resistance x constant input current
 duration = 1*second # duration of the simulation
 
 eqs = '''
- dv/dt = (v_rest - v + Rm_Ie)/tau_m : volt (unless refractory)
- '''
+      dv/dt = (v_rest - v + Rm_Ie)/tau_m : volt (unless refractory)
+      '''
 
 lif = NeuronGroup(N, model=eqs, threshold='v>v_th', reset='v=v_r', refractory=2*ms)
 lif.v = -67 * mV
