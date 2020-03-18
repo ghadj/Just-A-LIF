cf60adb393219bba2451d9d555c3030e2f964c60
georgehadjiantonis@hotmail.com <georgehadjiantonis@hotmail.com>
2020-03-18 18:48:30 +0200

    addition of comments, simulation outcomes and write results to csv files

--- src/io/github/ghadj/LIFVisualCorex/LIFDriver.java
+++ src/io/github/ghadj/LIFVisualCorex/LIFDriver.java
@@ -7,21 +7,27 @@ import java.util.List;
 /**
  * Driver for LIF class.
  * 
+ * Compile from LIF-Visual-Cortex/ directory
+ * javac -d ./bin ./src/io/github/ghadj/LIFVisualCortex/*.java
+ * 
+ * Run from Simple-Neural-Network/ directory
+ * java -cp ./bin io.github.ghadj.LIFVisualCortex.LIFNetworkDriver
+ * 
  * @author George Hadjiantonis
- * @since 17-03-2020
+ * @since 18-03-2020
  */
 public class LIFDriver {
 	// Set Constants
-	public static final double tau_m = 30; // membrane time constant - msec
+	public static final double tau_m = 30; // membrane time constant (msec)
 	public static final double Vreset = -65; // reset potential - mV
-	public static final double Vrest = Vreset; // aka reversal potential of the leakage (E_L) - mV
-	public static final double Vth = -50; // threshold potential - mV
-	public static final double V0 = -67; // Membrane potential at time 0 - mV
-	public static final double Rm = 90; // membrane resistance - MΩ
-	public static final double Ie = 1; // constant injected current - nA
-	public static final double Trefract = 2; // absolute refractory period - msec
-	public static final double Dt = 0.25; // Δt - time between two consecutive points
-	public static final double duration = 1000; // duration of the simulation - msec
+	public static final double Vrest = Vreset; // aka reversal potential of the leakage (EL) (mV)
+	public static final double Vth = -50; // threshold potential (mV)
+	public static final double V0 = -67; // Membrane potential at time 0 (mV)
+	public static final double Rm = 90; // membrane resistance (MΩ)
+	public static final double Ie = 1; // constant injected current (nA)
+	public static final double Trefract = 2; // absolute refractory period (msec)
+	public static final double Dt = 0.15; // Δt - time between two consecutive points (msec)
+	public static final double duration = 1000; // duration of the simulation (msec)
 
 	/**
 	 * @param args
@@ -31,14 +37,23 @@ public class LIFDriver {
 		LIF model = new LIF(tau_m, Vreset, Vrest, Vth, Rm, Trefract);
 
 		model.run(V0, Ie, duration, Dt);
-		System.out.println("spikes #" + model.numSpikes());
 
-		FileWriter writer = new FileWriter("./ouput.csv");
+		FileWriter writerPotential = new FileWriter("./potential.csv");
+		FileWriter writerSpikes = new FileWriter("./spikes.csv");
+
 		List<Double> potential = model.getPotential();
-		if (potential != null) {
-			String output = potential.toString().replaceAll("\\[|\\]", "").replaceAll(",", "\n");
-			writer.write(output);
+		if (potential != null)
+			writerPotential.write(potential.toString().replaceAll("\\[|\\]", "").replaceAll(",", "\n"));
+
+		List<Double> spikes = model.getSpikes();
+		if (spikes != null && !spikes.isEmpty()) {
+			System.out.printf("First spike at %.2f msec.\n", spikes.get(0));
+			writerSpikes.write(spikes.toString().replaceAll("\\[|\\]", "").replaceAll(",", "\n"));
+
+			System.out.printf("Number of spikes during the simulation %d.\n", model.numSpikes());
+			System.out.printf("Mean ISI %.2f msec.\n", duration / model.numSpikes());
 		}
-		writer.close();
+		writerPotential.close();
+		writerSpikes.close();
 	}
 }
